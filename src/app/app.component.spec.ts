3471fb1afbb2be432886ea7799d76cf9097a280b
Maratib Ali Khan <maratib@gmail.com>
2024-12-10 05:22:24 +0500

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ang9' title`, () => {
+  it(`should have the 'project-name' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang9');
+    expect(app.title).toEqual('project-name');
   });
 
   it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ang9');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, project-name');
   });
 });
